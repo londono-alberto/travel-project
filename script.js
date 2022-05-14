90ca246e38089ae25fe610b2c77afda880f3042f
alberto londono <albertolondono.londono5@gmail.com>
2022-05-14 14:08:03 -0500

    updated files for mvp

--- script.js
+++ script.js
@@ -1,90 +1,30 @@
 //api tests
 
-testApiWeather();
-function testApiWeather () {
-  const apiKey = 'b6a631faf48ec36736fa91299da2f0a2';
+//keys
+var apiKeyWeather = 'b6a631faf48ec36736fa91299da2f0a2';
+var apiKeyNPS = 'UwI3kgigKGVdm8bk9XTQmiupY45dyxNZfIcdn81Q';
+var apiGoogleMaps = 'AIzaSyD4OVkkkHA93ViisjQDq3Fx_oAtNuevgR0'
 
-  var cityInput = $('#cityInput').val();
-  console.log(cityInput);
+
+
+parkSearch();
+function parkSearch () {
+
+  var activityInput = document.querySelector('.activity-input')
+  console.log(activityInput);
 
   $.ajax({
+    
 
           type: "GET",
-          url: `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&APPID=${apiKey}`,
-          id: "city",
+          url: 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=UwI3kgigKGVdm8bk9XTQmiupY45dyxNZfIcdn81Q',
+          
           success: function(data) {
-            
-            
             console.log(data);
+
+            var activityEl = data.data[0].activities[0].name;
+            console.log(activityEl);
 
           }
         });
       }
-
-
-// testApiTrails();
-// function testApiTrails () {
-
-
-//   $.ajax({
-
-//           type: "GET",
-//           id: 2,
-//           url: 'https://prescriptiontrails.org/api/trail/?id=2',
-          
-//           success: function(data) {
-            
-            
-//             console.log(data);
-
-//           }
-//         });
-//       }
-
-
-
-
-// function testHikingApi () {
-//   var myHeaders = new Headers();
-//   myHeaders.append("Content-Type", "application/json");
-  
-//   var requestOptions = {
-//     method: 'GET',
-//     headers: myHeaders,
-//     redirect: 'follow',
-//     mode: "no-cors"
-//   };
-//   console.log(requestOptions);
-//   fetch("https://www.benbrougher.tech/hiker/v1/trails/", requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
-// }
-
-
-
-
-
-
-$('#submitBtn').click(function(){
-    testApiWeather();
-    console.log(cityInput);
-  });
-  //search with enter key
-// $('#cityInput').keypress(function(e){
-//   if(e.which === 13) {
-//     testApiWeather();
-//   }
-// });
-
-
-// $('#submitBtnTwo').click(function(){
-//     testApiTrails();
-
-//   });
-//   //search with enter key
-// $('#trailInput').keypress(function(e){
-//   if(e.which === 13) {
-//     testApiTrailsr();
-//   }
-// });
