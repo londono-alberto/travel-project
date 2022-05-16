9208254979b43702e59c3636cf56f882a33fa791
Mason Benson <35643709+mbenson025@users.noreply.github.com>
2022-05-15 22:13:06 -0500

    Update script.js
    script corrected

--- script.js
+++ script.js
@@ -1,124 +1,120 @@
 // web api keys
-var apiKeyWeather = 'b6a631faf48ec36736fa91299da2f0a2';
-var apiKeyNPS = 'UwI3kgigKGVdm8bk9XTQmiupY45dyxNZfIcdn81Q';
-var apiGoogleMaps = 'AIzaSyD4OVkkkHA93ViisjQDq3Fx_oAtNuevgR0'
+var apiKeyWeather = "b6a631faf48ec36736fa91299da2f0a2";
+var apiKeyNPS = "UwI3kgigKGVdm8bk9XTQmiupY45dyxNZfIcdn81Q";
+var apiGoogleMaps = "AIzaSyD4OVkkkHA93ViisjQDq3Fx_oAtNuevgR0";
 
-let startBtn = document.getElementById('stateButton');
-let returnBtn = document.getElementById('returnButton');
-let activityCard = document.getElementById('activity-card');
+let startBtn = document.getElementById("stateButton");
+let returnBtn = document.getElementById("returnButton");
+let activityCard = document.getElementById("activity-card");
 
-
-// button that gets the value fromt the dropdown list 
-startBtn.addEventListener('click', function(){
-  let userInput = $('#myDropdown :selected').val();
+// button that gets the value fromt the dropdown list
+startBtn.addEventListener("click", function () {
+  let userInput = $("#myDropdown :selected").val();
   console.log(userInput);
 
-  // hides the dropdown list and the button 
-  $('.dropdown').hide()
-  $('.returnButton').show()
+  // hides the dropdown list and the button
+  $(".dropdown").hide();
+  $(".returnButton").show();
 
-  stateSearch(userInput)
-})
+  stateSearch(userInput);
+});
 
-returnBtn.addEventListener('click', function(){
+returnBtn.addEventListener("click", function () {
   location.reload();
-})
+});
 
-// this function will fetch the data for the google maps and display the activities 
-function stateSearch (state) {
-  $('#map').show();
+// this function will fetch the data for the google maps and display the activities
+function stateSearch(state) {
+  $("#map").show();
 
   $.ajax({
-    
+    type: "GET",
+    url:
+      "https://developer.nps.gov/api/v1/parks?parkCode=" +
+      state +
+      "&api_key=" +
+      apiKeyNPS,
 
-          type: "GET",
-          url: "https://developer.nps.gov/api/v1/parks?parkCode=" + state + "&api_key=" + apiKeyNPS, 
-          
-          
-          success: function(data) {
-            console.log(data);
-            
+    success: function (data) {
+      console.log(data);
 
-            // attempting to display hawaii on array 6 and create a p element to append the url text
-            for (let i = 0; i < data.data.length; i++){
-            if (state === data.data[i].states) {
-              // this variable will contain the coordinates for the google maps api 
-              let {latitude, longitude} = data.data[i];
-              let {url} = data.data[i];
-              
-              // creates p elements 
-              let createP = document.createElement('p');
-              let createP2 = document.createElement('p');
-              $(createP2).html($(`<a href="${url}">Link to ${data.data[i].name} Park</a>`));
-              
-              // creates div elements to append other elements to 
-              let createDiv = document.createElement('div');
-              let createDiv2 = document.createElement('div');
+      // attempting to display hawaii on array 6 and create a p element to append the url text
+      for (let i = 0; i < data.data.length; i++) {
+        if (state === data.data[i].states) {
+          // this variable will contain the coordinates for the google maps api
+          let { latitude, longitude } = data.data[i];
+          let { url } = data.data[i];
 
-              // this div will append the url link -- needs to be here so it doesnt get created multiple times 
-              let createDiv3 = document.createElement('div');
-              createDiv3.append(createP2);
-              activityCard.append(createDiv3);
+          // creates p elements
+          let createP = document.createElement("p");
+          let createP2 = document.createElement("p");
+          $(createP2).html(
+            $(`<a href="${url}">Link to ${data.data[i].name} Park</a>`)
+          );
 
-              // this creates an image tag with the src attribute for the image 
-              let createImg = document.createElement('img');
-              createImg.setAttribute('src', "")
-              createImg.src = data.data[i].images[0].url
-              createDiv2.append(createImg);
-              activityCard.append(createDiv2);
-              
-              // this for loop specifies the array within the data array 
-              for (let j = 0; j < data.data[i].activities.length; j++){
+          // creates div elements to append other elements to
+          let createDiv = document.createElement("div");
+          let createDiv2 = document.createElement("div");
 
-                // these elements get the specified data 
-                createP = data.data[i].activities[j].name
-                
+          // this div will append the url link -- needs to be here so it doesnt get created multiple times
+          let createDiv3 = document.createElement("div");
+          createDiv3.append(createP2);
+          activityCard.append(createDiv3);
 
-                // these elements are getting appeneded to separate divs 
-                createDiv.append(createP);
-                
-              
-                // these elements are getting appended to the card 
-                
-                activityCard.append(createDiv);
-                
+          // this creates an image tag with the src attribute for the image
+          let createImg = document.createElement("img");
+          createImg.setAttribute("src", "");
+          createImg.src = data.data[i].images[0].url;
+          createDiv2.append(createImg);
+          activityCard.append(createDiv2);
 
-              }
+          // this for loop specifies the array within the data array
+          for (let j = 0; j < data.data[i].activities.length; j++) {
+            // these elements get the specified data
+            createP = data.data[i].activities[j].name;
 
-              // this invokes the apiGoogleMaps with the specified coords 
-              mapApi(latitude, longitude);
-            }
+            // these elements are getting appeneded to separate divs
+            createDiv.append(createP);
+
+            // these elements are getting appended to the card
+
+            activityCard.append(createDiv);
           }
-          }
-        });
+
+          // this invokes the apiGoogleMaps with the specified coords
+          mapApi(latitude, longitude);
+        }
       }
+    },
+  });
+}
 
-      function mapApi(lat, lon) {
-        const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        mapId: "2f72557b09a6245f"
+function mapApi(lat, lon) {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    mapId: "2f72557b09a6245f",
+  });
+  console.log(lat, lon);
+  const geocoder = new google.maps.Geocoder();
+
+  geocoder
+    .geocode({ address: "US" })
+    .then((response) => {
+      const position = response.results[0].geometry.location;
+
+      map.setCenter(new google.maps.LatLng(lat, lon));
+      new google.maps.Marker({
+        map,
+        position,
       });
-      console.log(lat, lon);
-      const geocoder = new google.maps.Geocoder();
-    
-      geocoder
-        .geocode({ address: "US" })
-        .then((response) => {
-          const position = response.results[0].geometry.location;
-    
-          map.setCenter(new google.maps.LatLng(lat, lon));
-          new google.maps.Marker({
-            map,
-            position,
-          });
-        })
-        .catch((e) =>
-          window.alert("Geocode was not successful for the following reason: " + e)
-        );
-    }
-    
-    // hides elements until function is called 
-    $(document).ready(function(){
-      $('#map').hide();
-      $('.returnButton').hide()
     })
+    .catch((e) =>
+      window.alert("Geocode was not successful for the following reason: " + e)
+    );
+}
+
+// hides elements until function is called
+$(document).ready(function () {
+  $("#map").hide();
+  $(".returnButton").hide();
+});
