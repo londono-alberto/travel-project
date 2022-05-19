e7d795e9d8bb18da87a66b9cf6e8a9598963b8d4
alberto londono <albertolondono.londono5@gmail.com>
2022-05-19 16:08:11 -0500

    committing trial

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,6 +38,7 @@ startBtn.addEventListener("click", function () {
   $(".park-choice").show();
 });
 
+// button to return to state selection 
 returnBtn.addEventListener("click", function () {
   parkImages = [];
   carousel.style.backgroundImage = "url('https://picsum.photos/300/200')";
@@ -76,59 +77,6 @@ parkBtn.addEventListener("click", function (e) {
   $(".park-choice").hide();
 });
 
-// global variables that link to the carousel function
-var carousel = document.querySelector(".carouselbox");
-
-// these variables are connected to the carousel
-var next = carousel.querySelector(".next");
-var prev = carousel.querySelector(".prev");
-var index = 0;
-var currentImage;
-
-var parkImages = [];
-
-carousel.style.backgroundImage = "url('https://picsum.photos/300/200')";
-
-// carousel function to rotate pictures through
-// function holds the direction from the user input and assigns it a value to perform the function
-function navigate(direction) {
-  // index stores the value of the direction
-  index += direction;
-
-  // once the index stores the value of the index, the if else function runs
-
-  if (index < 0) {
-    index = parkImages.length - 1;
-  } else if (index > parkImages.length - 1) {
-    index = 0;
-  }
-  currentImage = parkImages[index];
-  carousel.style.backgroundImage = "url('" + currentImage + "')";
-}
-
-// When this is clicked it rotates through the index of pictures in the array
-carousel.addEventListener("click", function () {
-  window.location.href = parkImages[index];
-});
-
-// this is connected to the next button. adds to the index by 1
-next.addEventListener("click", function (event) {
-  // Prevents it from event bubbling. So when clicked, it doesnt also click the image
-  event.stopPropagation();
-
-  // invokes the function with an argument incrementing it by 1
-  navigate(1);
-});
-
-// this is connected to the next button. decrements the index by 1
-prev.addEventListener("click", function (event) {
-  // Prevents it from event bubbling. So when clicked, it doesnt also click the image
-  event.stopPropagation();
-
-  // invokes the function with an argument decrementing it by 1
-  navigate(-1);
-});
-
 // beginning of the localstorage functions --------vvvvv
 
 // empty array to push the userInput
@@ -145,6 +93,7 @@ function init() {
   renderTodos();
 }
 
+// creates buttons from the localstorage value to be appended onto the page 
 function renderTodos() {
   // This clears the search history log
   searchBoard.innerHTML = "";
@@ -200,12 +149,14 @@ searchBoard.addEventListener("click", function (event) {
   }
 });
 
+// clears localStorage and refreshes the page 
 clearBtn.addEventListener("click", clearParks);
 function clearParks() {
   localStorage.clear();
   window.location.reload();
 }
 
+// hides and shows buttons when wanted 
 $(document).ready(function () {
   $("#map").hide();
   $(".returnButton").hide();
