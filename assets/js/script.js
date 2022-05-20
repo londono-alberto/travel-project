87bbfb0e3676982a65596457e60a0d21404ad328
mbenson025 <mbenson025@gmail.com>
2022-05-19 19:47:21 -0500

    fixed map starting before call

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,6 +62,8 @@ parkBtn.addEventListener("click", function (e) {
   var parkSave = e.target.textContent;
   var parkEl = $(element).val();
 
+  // localStorage
+
   // Pushes the text into the localstorage array
   searchArray.push(parkSave);
 
@@ -74,59 +76,6 @@ parkBtn.addEventListener("click", function (e) {
   $("#parkList").hide();
   $(".container").show();
   $(".park-choice").hide();
-});
-
-// global variables that link to the carousel function
-var carousel = document.querySelector(".carouselbox");
-
-// these variables are connected to the carousel
-var next = carousel.querySelector(".next");
-var prev = carousel.querySelector(".prev");
-var index = 0;
-var currentImage;
-
-var parkImages = [];
-
-carousel.style.backgroundImage = "url('https://picsum.photos/300/200')";
-
-// carousel function to rotate pictures through
-// function holds the direction from the user input and assigns it a value to perform the function
-function navigate(direction) {
-  // index stores the value of the direction
-  index += direction;
-
-  // once the index stores the value of the index, the if else function runs
-
-  if (index < 0) {
-    index = parkImages.length - 1;
-  } else if (index > parkImages.length - 1) {
-    index = 0;
-  }
-  currentImage = parkImages[index];
-  carousel.style.backgroundImage = "url('" + currentImage + "')";
-}
-
-// When this is clicked it rotates through the index of pictures in the array
-carousel.addEventListener("click", function () {
-  window.location.href = parkImages[index];
-});
-
-// this is connected to the next button. adds to the index by 1
-next.addEventListener("click", function (event) {
-  // Prevents it from event bubbling. So when clicked, it doesnt also click the image
-  event.stopPropagation();
-
-  // invokes the function with an argument incrementing it by 1
-  navigate(1);
-});
-
-// this is connected to the next button. decrements the index by 1
-prev.addEventListener("click", function (event) {
-  // Prevents it from event bubbling. So when clicked, it doesnt also click the image
-  event.stopPropagation();
-
-  // invokes the function with an argument decrementing it by 1
-  navigate(-1);
 });
 
 // beginning of the localstorage functions --------vvvvv
@@ -200,6 +149,26 @@ searchBoard.addEventListener("click", function (event) {
   }
 });
 
+//  {'parks', [
+//         {state: "CA", code: "1111"}
+//          {state: "CA", code: "2222"}
+//          {state: "CA", code: "3333"}
+//         ]}
+
+$(".favBtn").on("click", favoritePark);
+
+function favoritePark(e) {
+  $(this).find(".fa").removeClass("fa-tree").addClass("fa-check");
+  setTimeout(() => {
+    $(this).find(".fa").removeClass("fa-check").addClass("fa-tree");
+  }, 400);
+
+  //set key
+  //set value
+
+  localStorage.setItem(k, v);
+}
+
 clearBtn.addEventListener("click", clearParks);
 function clearParks() {
   localStorage.clear();
@@ -214,4 +183,3 @@ $(document).ready(function () {
   $(".search-header").hide();
   $(".clearBtn").show();
 });
-
